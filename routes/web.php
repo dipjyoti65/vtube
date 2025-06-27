42388d449b12e42fb842fe8436ff37a0725bafec
Dip jyoti gayan <119562482+dipjyoti65@users.noreply.github.com>
2025-06-27 20:54:17 +0530

    web api updated

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\VideoController;
 
 Route::get('/', function () {
     // return view('welcome');
      return 'Laravel is working!';
 });
+
+Route::get('/get-all-videos', [VideoController::class, 'getAllVideos']);
